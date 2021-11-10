24cf0e7debc05e7f43fd0100f6f6a9b7fd78f218
Brady <brady.peters@keyin.com>
2021-11-10 10:22:59 -0330

    Added comments

---
+++ main/java/com/keyin/domain/appointment/BloodDonationAppointment.java
@@ -0,0 +1,74 @@
+package com.keyin.domain.appointment;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class BloodDonationAppointment {
+    private int appointmentId;
+    private LocalDate appointmentDate;
+    private LocalTime appointmentTime;
+    private int duration;
+    private String location;
+    private String bloodType;
+    private boolean firstTimeDonor;
+    private int donorID;
+//Setters for Appointment
+    public void setAppointmentId(int appointmentId) { this.appointmentId = appointmentId; }
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+    public void setAppointmentTime(LocalTime appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public void setBloodType(String bloodType)
+    { this.bloodType = bloodType;
+    }
+    public void setFirstTimeDonor(boolean firstTimeDonor) {
+        this.firstTimeDonor = firstTimeDonor;
+    }
+    public void setDonorID(int donorID) {
+        this.donorID = donorID;
+    }
+    public void setAppointmentInfo(int appointmentId, LocalDate appointmentDate, LocalTime appointmentTime,int duration, String location,
+                                   String bloodType, boolean firstTimeDonor, int donorID) {
+        this.appointmentId = appointmentId;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+        this.duration = duration;
+        this.location = location;
+        this.bloodType = bloodType;
+        this.firstTimeDonor = firstTimeDonor;
+        this.donorID = donorID;
+    }
+//Getters for Appointment
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+    public LocalDate getAppointmentDate() {
+        return appointmentDate;
+    }
+    public LocalTime getAppointmentTime() {
+        return appointmentTime;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public String getBloodType() {
+        return bloodType;
+    }
+    public boolean isFirstTimeDonor() {
+        return firstTimeDonor;
+    }
+    public int getDonorID() {
+        return donorID;
+    }
+}
